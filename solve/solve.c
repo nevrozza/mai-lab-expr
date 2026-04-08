959fa292eefcf0345077d61642eb0093dc745074
Matashkov Artyom <artemker667@gmail.com>
2026-04-08 16:58:29 +0300

    Add `build_tree_from_queue`

--- solve/solve.c
+++ solve/solve.c
@@ -1,11 +1,25 @@
 #include "solve.h"
 
+#include <stdlib.h>
+#include "../utils/utils.h"
 #include "../structures/stack_generic.h"
 
 static int get_priority(const char sym) {
     if (sym == '*' || sym == '/') return 2;
     if (sym == '+' || sym == '-') return 1;
     return 0;
+}
+
+static Node *create_node(Token t, Node *r, Node *l) {
+    Node *n = malloc(sizeof(Node));
+    if (!n) {
+        fatal_memory_error();
+        return NULL;
+    }
+    n->tok = t;
+    n->left = l;
+    n->right = r;
+    return n;
 }
 
 // shunting yard (Deikstra)
@@ -47,4 +61,25 @@ Queue *postfix_queue_from_input_expr() {
     while (!TokenStack_is_empty(ops)) queue_push(output, TokenStack_pop(ops));
     TokenStack_destroy(ops);
     return output;
+}
+
+
+Node *build_tree_from_queue(Queue *q) {
+    if (!q) return NULL;
+    NodeStack *s = NodeStack_create();
+
+    while (!queue_is_empty(q)) {
+        const Token t = queue_pop(q);
+
+        if (t.type == TOK_VAL || t.type == TOK_NAME) {
+            NodeStack_push(s, create_node(t, NULL, NULL));
+        } else {
+            Node *parent = create_node(t, NodeStack_pop(s), NodeStack_pop(s));
+            NodeStack_push(s, parent);
+        }
+    }
+
+    Node *root = NodeStack_pop(s);
+    NodeStack_destroy(s);
+    return root;
 }
--- solve/solve.h
+++ solve/solve.h
@@ -5,4 +5,6 @@
 
 Queue *postfix_queue_from_input_expr();
 
+Node *build_tree_from_queue(Queue *q);
+
 #endif //EXPR_SOLVE_H
