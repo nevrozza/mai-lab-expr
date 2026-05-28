51fac6b0e5e37df5fc2e3177ed5dc9b0553a3c85
Matashkov Artyom <artemker667@gmail.com>
2026-05-28 23:56:39 +0300

    add support of implicit multiplication

--- solve/tree_from_expr.c
+++ solve/tree_from_expr.c
@@ -22,13 +22,56 @@ static Node *create_node(Token t, Node *r, Node *l) {
     return n;
 }
 
+static void process_sym_token(Token t, TokenStack *ops, Queue *output) {
+    if (t.data.sym == '(') {
+        TokenStack_push(ops, t);
+    } else if (t.data.sym == ')') {
+        while (!TokenStack_is_empty(ops) &&
+               (TokenStack_peek(ops).type != TOK_SYM || TokenStack_peek(ops).data.sym != '(')) {
+            queue_push(output, TokenStack_pop(ops));
+        }
+        if (!TokenStack_is_empty(ops)) TokenStack_pop(ops);
+    } else {
+        while (!TokenStack_is_empty(ops) &&
+               TokenStack_peek(ops).type == TOK_SYM &&
+               TokenStack_peek(ops).data.sym != '(' &&
+               get_priority(TokenStack_peek(ops).data.sym) >= get_priority(t.data.sym)) {
+            queue_push(output, TokenStack_pop(ops));
+        }
+        TokenStack_push(ops, t);
+    }
+}
+
 // shunting yard (Deikstra)
 Queue *postfix_queue_from_input_expr() {
     TokenStack *ops = TokenStack_create();
     Queue *output = queue_create();
     Token t;
 
+
+    int prev_type = -1;
+    char prev_sym = '\0';
+
     while (read_next_token(&t)) {
+        // Неявное умножение
+        if (prev_type != -1) {
+            bool prev_before_mul = (prev_type == TOK_VAL ||
+                                    prev_type == TOK_VAR ||
+                                    (prev_type == TOK_SYM && prev_sym == ')'));
+
+            bool curr_after_mul = (t.type == TOK_VAL ||
+                                   t.type == TOK_VAR ||
+                                   (t.type == TOK_SYM && t.data.sym == '('));
+
+            if (prev_before_mul && curr_after_mul) {
+                const Token mul_t = {TOK_SYM, '*'};
+                process_sym_token(mul_t, ops, output);
+            }
+        }
+        prev_type = t.type;
+        prev_sym = t.type == TOK_SYM ? t.data.sym : '\0';
+
+
         switch (t.type) {
             case TOK_VAL:
             case TOK_VAR:
@@ -36,23 +79,7 @@ Queue *postfix_queue_from_input_expr() {
                 break;
 
             case TOK_SYM:
-                if (t.data.sym == '(') {
-                    TokenStack_push(ops, t);
-                } else if (t.data.sym == ')') {
-                    while (!TokenStack_is_empty(ops) &&
-                           (TokenStack_peek(ops).type != TOK_SYM || TokenStack_peek(ops).data.sym != '(')) {
-                        queue_push(output, TokenStack_pop(ops));
-                    }
-                    if (!TokenStack_is_empty(ops)) TokenStack_pop(ops);
-                } else {
-                    while (!TokenStack_is_empty(ops) &&
-                           TokenStack_peek(ops).type == TOK_SYM &&
-                           TokenStack_peek(ops).data.sym != '(' &&
-                           get_priority(TokenStack_peek(ops).data.sym) >= get_priority(t.data.sym)) {
-                        queue_push(output, TokenStack_pop(ops));
-                    }
-                    TokenStack_push(ops, t);
-                }
+                process_sym_token(t, ops, output);
                 break;
             default:
                 break;
